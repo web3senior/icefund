3216368f1867c64e89e59b65d04360bd8c53894f
web3senior <atenyun@gmail.com>
2025-03-21 04:03:02 +0330

    Update contracct

--- contracts/Event.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-event Log(string message);
-event FeeUpdated(uint256);
-event Locked(address indexed token, uint256 amount, uint256 expiration, address indexed locker  ,uint256 dt, bool indexed status);
-event UnLocked(address indexed token, uint256 amount, uint256 expiration, address indexed locker  ,uint256 dt, bool indexed status);
